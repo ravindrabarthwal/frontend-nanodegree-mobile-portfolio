382169109dede47573f0bbd162a75321dd1c4036
Ravindra barthwal <rtechshow@gmail.com>
2016-04-04 13:39:59 +0530

    Optimizing performance and shipping for github pages

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,11 +1,10 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
+var stats = document.getElementById("crp-stats");
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
     complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
